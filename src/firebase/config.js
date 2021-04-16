eb448e321f2358e194f0910be631f9521148a836
tushardahiya <tushardahiya84@gmail.com>
2021-04-16 18:31:43 +0530

    setup firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAodhiVdPqmF3KWStUMgTn2VlJOhk8fFpc",
+  authDomain: "tushargram-33038.firebaseapp.com",
+  projectId: "tushargram-33038",
+  storageBucket: "tushargram-33038.appspot.com",
+  messagingSenderId: "631633621814",
+  appId: "1:631633621814:web:c535c4a45a3dc67e7cf997",
+  measurementId: "G-J1V3K2G3XY",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
