3d0cd5764c0ac48a06e3f1464c2632872d96e506
tushardahiya <tushardahiya84@gmail.com>
2021-04-17 12:40:37 +0530

    created firestore custom hook to actively listen to changes and fetch images + added component for showing images

---
+++ src/comps/ImageGrid.js
@@ -0,0 +1,20 @@
+import React from "react";
+import useFirestore from "../hooks/useFirestore";
+
+const ImageGrid = () => {
+  const { docs } = useFirestore("images");
+  console.log(docs);
+
+  return (
+    <div className="image-grid">
+      {docs &&
+        docs.map((doc) => (
+          <div className="image-wrap" key={doc.id}>
+            <img src={doc.url} alt="fetched pictures" />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default ImageGrid;
